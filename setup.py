5d401d4e7be7553a9ae9dc1884d28296e0c65abc
Kenneth Nicholas Jørgensen <71528623+DJ-Dingo@users.noreply.github.com>
2023-08-05 00:33:33 +0200

    Update setup.py v2.0.2

--- setup.py
+++ setup.py
@@ -6,45 +6,10 @@ from io import open
 import os.path as osp
 from setuptools import setup
 from setuptools.command.install import install
-import shutil
-import os
 
 HERE = osp.abspath(osp.dirname(__file__))
 sys.path.insert(0, HERE)
 from pibooth_oled_display import pibooth_oled_display as plugin
-
-class CustomInstallCommand(install):
-    def run(self):
-        install.run(self)
-
-        # Custom file copying script
-        source_dir = os.path.join(HERE, 'pibooth_oled_display', 'oled_display')
-        destination_dir = os.path.expanduser('~/.config/pibooth/oled_display')
-
-        print("Source directory: ", source_dir)
-        print("Destination directory: ", destination_dir)
-
-        if not os.path.exists(destination_dir):
-            os.makedirs(destination_dir)
-
-        # Define ignored files and directories
-        ignored_items = ['__pycache__', '__init__.py', 'pibooth_oled_display.py']
-
-        for root, dirs, files in os.walk(source_dir):
-            # Don't copy undesired files and directories
-            dirs[:] = [d for d in dirs if d not in ignored_items]
-            files = [f for f in files if f not in ignored_items]
-            
-            for file_name in files:
-                source_path = os.path.join(root, file_name)
-                destination_path = source_path.replace(source_dir, destination_dir)
-
-                print("Copying: ", source_path, " to ", destination_path)
-                
-                os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-                shutil.copy(source_path, destination_path)
-
-
 
 def main():
     setup(
@@ -83,6 +48,7 @@ def main():
             'pillow',
             'pibooth',
             'pibooth plugin',
+            'oled display',
             'luma.oled',
             'oled'
         ],
@@ -91,42 +57,24 @@ def main():
             'pibooth>=2.0.3',
             'luma.oled>=3.12.0'
         ],
-
-
-
-
-        
         include_package_data=True,
         packages=['pibooth_oled_display', 'pibooth_oled_display.oled_display'],
-package_data={
-    'pibooth_oled_display': ['*.json', '*.txt'],
-    'pibooth_oled_display.oled_display': ['logo/*', 'fonts/*', '*.py', '*.rst',
-                                          'states/capture/*', 
-                                          'states/choose/*', 
-                                          'states/failure/*', 
-                                          'states/finished/*', 
-                                          'states/layout/*', 
-                                          'states/preview/*', 
-                                          'states/printer/*', 
-                                          'states/processing/*',
-                                          'states/capture/origin/*', 
-                                          'states/choose/origin/*', 
-                                          'states/failure/origin/*', 
-                                          'states/finished/origin/*', 
-                                          'states/layout/origin/*', 
-                                          'states/preview/origin/*', 
-                                          'states/printer/origin/*', 
-                                          'states/processing/origin/*']
-},
+        package_data={
+            'pibooth_oled_display.oled_display': ['logo/*', 'fonts/*', 'default/*', '*.py', '*.rst',
+                                                  'states/capture/*', 
+                                                  'states/choose/*', 
+                                                  'states/failure/*', 
+                                                  'states/finished/*', 
+                                                  'states/layout/*', 
+                                                  'states/preview/*', 
+                                                  'states/printer/*', 
+                                                  'states/processing/*']
+        },
         options={
-            'bdist_wheel':
-                {'universal': True}
+            'bdist_wheel': {'universal': True}
         },
         zip_safe=False,
         entry_points={'pibooth': ["pibooth_oled_display = pibooth_oled_display.pibooth_oled_display"]},
-        cmdclass={
-            'install': CustomInstallCommand,
-        },
     )
 
 if __name__ == '__main__':
